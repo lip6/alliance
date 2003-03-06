74a85d59039c0103e72f26d5f5b9b794cd5d95db
Francois Donnet <alliance-cvs@asim.lip6.fr>
2003-03-06 15:59:10 +0000

    *** empty log message ***

--- alliance/src/pcbs/src/emulbs/bstools.c
+++ alliance/src/pcbs/src/emulbs/bstools.c
@@ -146,13 +146,6 @@ static char* unbuf_papat(char* head, struct papat *ppapat)
            fprintf(stderr,
              "SYNCHRONISATION ERROR on Boundary-scan hardware!!!\n" );
            
-           /*erreur probable si rien ne passe*/
-           if ( parallel_num == 0 )
-           {
-             fprintf(stderr,
-               "Configuration file describing connections doesn't match hardware\n" );
-           }
-
            fprintf(stderr,
              "-----> STOP! next results will be wrong...\n");
          }
