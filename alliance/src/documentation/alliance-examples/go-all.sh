6cefc6956c0816269b6b18497e63a7882cf9d0c0
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2012-05-03 20:28:37 +0000

    Added back hadamard.

--- alliance/src/documentation/alliance-examples/go-all.sh
+++ alliance/src/documentation/alliance-examples/go-all.sh
@@ -16,7 +16,7 @@
 
  for B in addaccu16 adder4 amd2901 amd2901-vasy digi divcas4 \
           multi16b-reg multi4b multi8 multi8b pgcd sqrt32 sqrt8 \
-          mipsR3000 
+          mipsR3000 hadamard
  do
    if [ -d $B ]; then
      echo "----- BENCH $B ----- "
