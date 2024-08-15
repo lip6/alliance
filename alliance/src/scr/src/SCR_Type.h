7ca13b15455cc7b8325cc53182e68a82a520d3ea
Naohiko Shimizu <naohiko.shimizu@gmail.com>
2024-08-15 14:06:12 +0200

    adjust vertical supply wire

--- alliance/src/scr/src/SCR_Type.h
+++ alliance/src/scr/src/SCR_Type.h
@@ -63,9 +63,9 @@ extern int SXMODE;
 # define VSSOFFSET	((long ) (long)(MBK_WIDTH_VSS/2) * SCALE_X)
 # define VDDWIDTH	((long ) MBK_WIDTH_VDD * SCALE_X)
 # define VSSWIDTH	((long ) MBK_WIDTH_VSS * SCALE_X)
-# define RIGHTSUPPLY	((long )(MBK_TRACK_SPACING_ALU2 +  MBK_WIDTH_VDD/2) * SCALE_X)
-# define LEFTSUPPLY	((long )(MBK_TRACK_SPACING_ALU2 +  MBK_WIDTH_VDD/2) * SCALE_X)
-# define MIDDLESUPPLY	((long )(MBK_WIDTH_VDD/2) * SCALE_X)
+# define RIGHTSUPPLY	((long )(MBK_TRACK_SPACING_ALU2*4 +  MBK_WIDTH_VDD) * SCALE_X)
+# define LEFTSUPPLY	((long )(MBK_TRACK_SPACING_ALU2   +  MBK_WIDTH_VDD) * SCALE_X)
+# define MIDDLESUPPLY	((long )(MBK_TRACK_SPACING_ALU2) * SCALE_X)
 # define CELLAB	        ((long ) MBK_Y_SLICE * SCALE_X)
 
 typedef struct SCR_Figure 
